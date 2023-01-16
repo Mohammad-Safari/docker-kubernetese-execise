486050f2242d38da217b5cb1bf3d291f0d9444b6
M.safari <m.safari@Tosan.com>
2023-01-16 12:23:56 +0330

    [enhance]port setting from command line args

--- E01/Program.cs
+++ E01/Program.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 
+var PROJECT_AUTHOR = "MohammadSafari";
+
 var builder = WebApplication.CreateBuilder(args);
 
-var port = Environment.GetEnvironmentVariable("PROGRAM_PORT") ?? "8080";
+var getOption = (string argName) => args.SkipWhile(arg => arg != argName).Skip(1).FirstOrDefault();
+var portValidator = (string port) => port.All(char.IsDigit) ? port : throw new ArgumentException("Invalid port number: " + port);
+var port = getOption("--program-port") ?? Environment.GetEnvironmentVariable("PROGRAM_PORT") ?? "8080";
+portValidator(port);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -14,7 +19,7 @@ app.Urls.Add($"http://*:{port}");
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error");  
 }
 app.UseStaticFiles();
 
@@ -25,5 +30,6 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.MapGet("/hello", ([FromQuery(Name = "name")] string? name) => $"Hello {name ?? "Stranger"}");
+app.MapGet("/author", () => $"Hello {PROJECT_AUTHOR}");
 
 app.Run();
