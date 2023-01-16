04acbe284920a653470d50aa6645d0fd9e6530d5
M.safari <m.safari@Tosan.com>
2023-01-17 00:13:53 +0330

    [add]console and w3c format http logging services

--- E01/Program.cs
+++ E01/Program.cs
@@ -1,20 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.Logging;
 
 var PROJECT_AUTHOR = "MohammadSafari";
-
-var builder = WebApplication.CreateBuilder(args);
+var PROJECT_LOG_DIR = "/var/log/e01";
 
 var getOption = (string argName) => args.SkipWhile(arg => arg != argName).Skip(1).FirstOrDefault();
 var portValidator = (string port) => port.All(char.IsDigit) ? port : throw new ArgumentException("Invalid port number: " + port);
 var port = getOption("--program-port") ?? Environment.GetEnvironmentVariable("PROGRAM_PORT") ?? "8080";
 portValidator(port);
 
+var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddW3CLogging(logging => {
+    logging.LoggingFields = W3CLoggingFields.All;
+    logging.LogDirectory = PROJECT_LOG_DIR;
+});
 
 var app = builder.Build();
 
 app.Urls.Add($"http://*:{port}");
+
+app.UseW3CLogging();
+app.UseHttpLogging();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
