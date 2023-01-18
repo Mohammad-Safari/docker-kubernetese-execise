53b42f503c4d591777f86d56d8c75a3d4ea630d7
M.safari <m.safari@Tosan.com>
2023-01-18 12:27:24 +0330

    [add]required http endpoints

--- E01-spring/src/main/java/com/tosan/dockerkubernetesetest/e01/E01Application.java
+++ E01-spring/src/main/java/com/tosan/dockerkubernetesetest/e01/E01Application.java
@@ -2,12 +2,26 @@ package com.tosan.dockerkubernetesetest.e01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class E01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(E01Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(E01Application.class, args);
+    }
+
+    @GetMapping(value = "hello")
+    public String sayHello(@RequestParam(value = "name", required = false) String name) {
+        return "Hello " + (name != null ? name : "Stranger");
+    }
+
+    @GetMapping(value = "author")
+    public String getAuthor() {
+        return "MohammadSafari";
+    }
 
 }
